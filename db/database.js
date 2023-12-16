54f116af672cb80a87a1a6045fe9a4975a8b58a9
Kemény Vince <kemeny.vince@outlook.hu>
2023-12-16 13:26:54 +0100

    Send cd specific cd request for the server and store the response to the state

--- db/database.js
+++ db/database.js
@@ -1,20 +1,44 @@
-// this is for the postgre database
-// connect for the database
+// this is for the postgres database
 
+// connect for the database
 const {Pool} = require("pg");
+const {client_encoding} = require("pg/lib/defaults");
+const {errors} = require("pg-promise");
+const constants = require("constants");
+
 const pool = new Pool({
 	host: "localhost",
 	port: 5432,
 	database: "mycdcollection",
 	user: "postgres",
-	password: "asdQWERTZ",
+	password: "asdqwertz",
 	allowExitOnIdle: true
 })
 
-const AllCDs = () => {
-  
+// often used database requests
+// all cds
+async function AllCDs_DBQuery (){
+	// "inline variable"
+	return await pool.query("SELECT * FROM cd")
+}
+
+// one specific cd and details
+async function SpecificCds_DBQuery(cdId){
+	// Todo: an epic query here to search a specific cd
+	console.info(cdId)
+	let result = null;
+	try {
+		result = pool.query("SELECT cd.cover AS CDCover, cd.title AS CDTitle, cd.sleeve AS CDSleeve, cd.review AS CDReview, cd.genre AS CDGenre, cd.artist AS CDArtist, tracks.title AS TrackTitle FROM cd INNER JOIN tracks ON cd.id = tracks.album WHERE tracks.album = $1::integer", [cdId]);
+		return result;
+	}catch (e) {
+		result = e;
+	}
+	console.log(result);
+
+	return result;
+
 }
 
 module.exports ={
-	pool,
+	AllCDs_DBQuery, SpecificCds_DBQuery
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const database = require("../db/database")
-const {queryCD, pool} = require("../db/database");
+const {AllCDs, AllCDs_DBQuery, SpecificCds_DBQuery} = require("../db/database");
 const e = require("express");
 const {errors} = require("pg-promise");
 
@@ -11,33 +11,32 @@ router.get('/fetchallcds', async (req, res, next) => {
 	console.log("found fetchallcds url");
 	// database query
 	try {
-		const result =
-			await database.query("SELECT * FROM cd");
+		const query_result = await AllCDs_DBQuery();
 		res.setHeader('Content-Type', 'application/json')
-		res.status(200).json(result.rows)
+		res.status(200).json(query_result.rows)
 	} catch (err) {
 		console.error(err)
-		res.status(500).json({msg: "casual error happened"})
+		res.setHeader('Content-Type', 'application/json')
+		res.status(500).json({msg: err})
 	}
 });
 
 // GET - specific cd
 // Todo: Handle if the ID does not exist
-router.get("/cds/:cdItem", (req, res) => {
+router.get("/cds/:cdItem", async (req, res) => {
+	console.info(req.params);
 	console.log(`/cds/:cdItem param is ${req.params.cdItem}`);
 	const paramInt = parseInt(req.params.cdItem);
 // query for CD details
-	let CDDetails = null
-	pool.query("SELECT cd.cover AS CDCover, cd.title AS CDTitle, cd.sleeve AS CDSleeve, cd.review AS CDReview," +
-		" cd.genre AS CDGenre, cd.artist AS CDArtist, tracks.title AS TrackTitle FROM cd INNER JOIN tracks ON cd.id" +
-		" = tracks.album WHERE tracks.album = $1", [paramInt], (error, result) => {
-		if (error) {
-			res.json({msg: error})
-		}
-		CDDetails = result.rows
-	});
-	console.log(CDDetails)
-	res.status(200).json(CDDetails)
+	try {
+		let CDDetails = await SpecificCds_DBQuery(paramInt);
+		console.log(CDDetails.rows)
+		res.status(200).json(CDDetails)
+	}
+	catch (e) {
+		console.error(e)
+		res.status(500).json({msg: e})
+	}
 })
 
 module.exports = router;
