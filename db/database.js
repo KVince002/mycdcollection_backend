34a196051603c28bf9bcb1ceebb20dfc40fa17d8
Kemény Vince <kemeny.vince@outlook.hu>
2023-12-16 23:45:05 +0100

    New Endpoints and sql queries. For CdS and tracks and for count.

--- db/database.js
+++ db/database.js
@@ -27,7 +27,7 @@ async function SpecificCds_DBQuery(cdId){
 	// Todo: an epic query here to search a specific cd
 	let result = null;
 	try {
-		result = pool.query("SELECT cd.cover AS CDCover, cd.title AS CDTitle, cd.sleeve AS CDSleeve, cd.review AS CDReview, cd.genre AS CDGenre, cd.artist AS CDArtist, tracks.title AS TrackTitle FROM cd INNER JOIN tracks ON cd.id = tracks.album WHERE tracks.album = $1::integer", [cdId]);
+		result = await pool.query("SELECT cd.cover AS CDCover, cd.title AS CDTitle, cd.sleeve AS CDSleeve, cd.review AS CDReview, cd.genre AS CDGenre, cd.artist AS CDArtist, tracks.title AS TrackTitle FROM cd INNER JOIN tracks ON cd.id = tracks.album WHERE tracks.album = $1::integer", [cdId]);
 		return result;
 	}catch (e) {
 		result = e;
@@ -35,9 +35,28 @@ async function SpecificCds_DBQuery(cdId){
 	console.log(result);
 
 	return result;
+}
 
+// overall details
+async function OverallDetails(){
+	let result = null;
+	try {
+		result = await pool.query("SELECT COUNT(distinct cd.title) AS CD_count, COUNT(distinct tracks.title) AS Tracks_count FROM cd INNER JOIN tracks ON cd.id = tracks.album")
+		return result;
+	}catch (e) {
+		return e;
+	}
+}
+
+// specific cd
+async function Specific_cd_details_DBQuery(cdId){
+	try {
+		return await pool.query("SELECT * FROM cd WHERE cd.id = $1::integer;", [cdId]);
+	}catch (e) {
+		return e
+	}
 }
 
 module.exports ={
-	AllCDs_DBQuery, SpecificCds_DBQuery
+	AllCDs_DBQuery, SpecificCds_DBQuery, OverallDetails, Specific_cd_details_DBQuery
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const database = require("../db/database")
-const {AllCDs, AllCDs_DBQuery, SpecificCds_DBQuery} = require("../db/database");
+const {AllCDs, AllCDs_DBQuery, SpecificCds_DBQuery, OverallDetails, Specific_cd_details_DBQuery} = require("../db/database");
 const e = require("express");
-const {errors} = require("pg-promise");
+const {response, request, json} = require("express");
+
+
+// Every cd and tracks details
+/*
+	This is for double check how many tracks and cd I have in the database. This will be good for catching errors
+ */
+async function Details (){
+	let result = await OverallDetails();
+	console.info("Details updated");
+	console.log(result.rows)
+	return result.rows;
+}
+// This will run for the first time to get thing works
+let details = Details();
+// This will update the details (should update in every 1 min and a bit more)
+details = setInterval(Details, 61000000);
 
 /* GET pages about cds */
 // GET - all the cds from the database
@@ -21,7 +37,7 @@ router.get('/fetchallcds', async (req, res, next) => {
 	}
 });
 
-// GET - specific cd
+// GET - specific cd with all details
 // Todo: Handle if the ID does not exist
 router.get("/cds/:cdItem", async (req, res) => {
 	console.info(req.params);
@@ -38,5 +54,49 @@ router.get("/cds/:cdItem", async (req, res) => {
 		res.status(500).json({msg: e})
 	}
 })
+
+// todo get specific cd
+// get specific cd
+router.get("/cds/spec/cd/:cdId", async (request, response,  next) => {
+	console.info("cds/spec/cd/:cdId found with "+ request.params.cdId + "id")
+	const idParameter = request.params.cdId
+	try {
+		if (idParameter >= parseInt(details.cd_count) || idParameter<=0){
+			response.status(404).json({error: "Not found"})
+		}
+		const result = await Specific_cd_details_DBQuery(request.params.cdId)
+		response.status(200).json(result.rows)
+	}catch (e) {
+		response.status(500).json({mgs: e})
+	}
+})
+
+// todo get specific songs
+// get specific songs by album
+router.get("cds/spec/tracks/:cdId", async (request, response, next) => {
+	console.info("cds/spec/tracks/:cdId found with "+ request.params.cdId + " id")
+	const idParameter = parseInt(request.params.cdId)
+	try {
+		if (idParameter >= parseInt(details.cd_count) || idParameter<=0){
+			console.error(`Paramatered id outbound`)
+			response.status(404).json({error: "Not found"})
+		}
+		const result = await pool.query("SELECT * FROM tracks WHERE tracks.album = $1::integer", [idParameter])
+		response.status(200).json(result.rows)
+	}catch (e) {
+		response.status(404).json({msg:e})
+	}
+})
+
+// todo post new cd
+router.post("cds/newcd", async (request,response,next) =>{
+
+})
+
+// todo patch already cd
+
+// todo delete cd
+
+// todo profile handling (for me only)
 
 module.exports = router;
